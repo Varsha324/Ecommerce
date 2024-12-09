98d5c72a03b1bba1859d16d8c155521cf2acbbf5
Varsha324 <varshalatha20@gmail.com>
2024-12-09 11:55:59 +0530

    added offers and populars

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Popular from '../Components/Popular/Popular'
 import Hero from '../Components/Hero/Hero'
+import Offers from '../Components/Offers/Offers'
 const Shop = () => {
   return (
     <div>
     <Hero/>
+    <Popular/>
+    <Offers/>
+
     </div>
   )
 }

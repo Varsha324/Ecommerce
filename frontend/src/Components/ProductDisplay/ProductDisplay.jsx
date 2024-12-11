ff90e5991eb3d4fb914131a25936f8062b25bd2d
Varsha324 <varshalatha20@gmail.com>
2024-12-11 19:05:09 +0530

    complete frontend

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
+import { ShopContext } from '../../Context/ShopContext';
 const ProductDisplay = (props) => {
     const{product}=props;
+    const{addToCart} = useContext(ShopContext);
   return (
     <div className='productdisplay'>
         <div className='productdisply-left'>
@@ -50,7 +52,7 @@ const ProductDisplay = (props) => {
           </div>
      </div>
      <div className="button">
-     <div>ADD TO CART</div>
+     <div onClick={()=>{addToCart(product.id)} }>ADD TO CART</div>
      </div>
     <p className='productdisplay-right-category'><span>Category :</span> Women, T-Shirt, Croptop</p>
     <p className='productdisplay-right-category'><span>Tags :</span> Modern , Latest </p>
